a98120171c4af52265b346460dec15eb086c24c3
Adam Creeger <adamcreeger@gmail.com>
2013-03-23 08:51:21 -0700

    Initial attempt at adding FB SDK - been a while!

---
+++ public/javascripts/main.js
@@ -0,0 +1,18 @@
+window.fbAsyncInit = function() {
+  console.log("FB:",FB);
+  FB.init({
+    appId      : '495905673807221', // App ID
+    channelUrl : '//shrouded-hollows-1864.herokuapp.com/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+};
+// Load the SDK Asynchronously
+(function(d){
+  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  ref.parentNode.insertBefore(js, ref);
+ }(document));
